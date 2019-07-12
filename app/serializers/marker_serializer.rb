8fe953a888644175e851a75d3ae18abdac712d1a
Jessica Pope <jessica.pope.co@gmail.com>
2019-07-11 20:22:55 -0600

    altered serializer

--- app/serializers/marker_serializer.rb
+++ app/serializers/marker_serializer.rb
@@ -1,4 +1,4 @@
 class MarkerSerializer < ActiveModel::Serializer
-  attributes :id, :image_url, :latitude, :longitude
+  attributes :id, :image_url, :latitude, :longitude, :dog_id
   has_one :dog
 end
